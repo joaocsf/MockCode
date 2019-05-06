f147af2c472a1b465eed7da5e98a979275528d50
João Ferreira <noobcsf@hotmail.com>
2019-05-06 17:57:14 +0100

    Updated HTML generator, Removed CSS and Js from the main document

--- pipeline/code_generation/__init__.py
+++ pipeline/code_generation/__init__.py
@@ -1,2 +1,5 @@
 from .Generator import Generator
 from .GeneratorHTML import GeneratorHTML
+from .GeneratorHTMLLists import GeneratorHTMLLists
+from .GeneratorHTMLGRID import GeneratorHTMLGRID
+from .GeneratorHTMLGRIDV2 import GeneratorHTMLGRIDV2
