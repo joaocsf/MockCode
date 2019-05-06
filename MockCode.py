20f3216fedd46fe26eb23062302856ac6c89947e
João Ferreira <noobcsf@hotmail.com>
2019-05-06 18:08:11 +0100

    Updated MockCode pipeline to use new pre-processors

--- MockCode.py
+++ MockCode.py
@@ -1,4 +1,7 @@
 import os
+#logging.getLogger('tensorflow').setLevel(logging.NOTSET)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 import sys
 import importlib
 from PIL import Image
@@ -8,41 +11,95 @@ import time
 import cv2 as cv
 from pipeline import pipeline as PipeLine
 import json
+import numpy as np
+
+BANNER=""""
+
+-----------------=========================================-----------------
+
+{0}'||    ||'                 '||      {1}     ..|'''.|              '||          {2}
+{0} |||  |||    ...     ....   ||  ..  {1}   .|'     '    ...      .. ||    ....  {2}
+{0} |'|..'||  .|  '|. .|   ''  || .'   {1}   ||         .|  '|.  .'  '||  .|...|| {2}
+{0} | '|' ||  ||   || ||       ||'|.   {1}   '|.      . ||   ||  |.   ||  ||      {2}
+{0}.|. | .||.  '|..|'  '|...' .||. ||. {1}    ''|....'   '|..|'  '|..'||.  '|...' {2}
+
+-----------------=========================================-----------------
+""".format('\033[93m', '\033[92m', '\033[0m')
+
+PROCESSING_IMAGE = """
+\033[92m Processing Image...
+\033[0m
+"""
+
+SETUP ="""
+\033[92m Initializing
+\033[0m
+"""
+
+Processing ="""
+\033[92m Processing
+\033[0m
+"""
 
 class MockCode:
   def __init__(self, pipeline):
     self.pipeline=pipeline
-    cv.namedWindow('Image')
+    cv.namedWindow('Image', cv.WINDOW_NORMAL)
     cv.createTrackbar('type', 'Image', 0, 1, nothing) 
     cv.createTrackbar('kernel', 'Image', 11, 50, nothing) 
     cv.createTrackbar('weight', 'Image', 10, 50, nothing) 
-    cv.createTrackbar('kernel_e', 'Image', 0, 3, nothing) 
+    cv.createTrackbar('kernel_e', 'Image', 1, 3, nothing) 
   
   def video(self):
     print('Processing Video')
     cam = cv.VideoCapture(0)
+    cv.setWindowProperty('Image', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
+    cv.setWindowProperty('Image', cv.WND_PROP_FULLSCREEN, cv.WINDOW_NORMAL)
+    cv.waitKey(1)
 
     while True:
       _, img = cam.read()
       key = cv.waitKey(20)
+      img = self.adapt_image(img)
       cv.imshow('Image', img)
 
       if key == 27:
         break
       elif key == 32:
         cv.imshow('ScreenShot', img)
+        cv.waitKey(1)
+        print(Processing)
         self.__run_pipeline__(img)
+        cv.setWindowProperty('Image', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
+        cv.setWindowProperty('Image', cv.WND_PROP_FULLSCREEN, cv.WINDOW_NORMAL)
+        cv.waitKey(1)
   
   def image(self, image_path):
-    print('Processing Image')
+    print(PROCESSING_IMAGE, flush=True)
     image = cv.imread(image_path)
     cv.imshow('Input', image)
+    cv.waitKey(1)
+    image = self.adapt_image(image)
+    print(Processing)
     self.__run_pipeline__(image)
   
   def __run_pipeline__(self, image):
-    image = self.adapt_image(image)
     self.pipeline.execute(image)
     self.pipeline.show_generator_results()
+
+  def corp_image(self, img, rect):
+    center, size, angle = rect[0], rect[1], rect[2]
+    center, size = tuple(map(int, center)), tuple(map(int, size))
+    if angle < -45:
+      angle += 90
+    height, width = img.shape[0], img.shape[1]
+
+    M = cv.getRotationMatrix2D(center, angle, 1)
+    img_rot = cv.warpAffine(img, M, (width, height))
+
+    img_crop = cv.getRectSubPix(img_rot, size, center)
+
+    return img_crop, img_rot
 
   def adapt_image(self, img):
     img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
@@ -61,10 +118,38 @@ class MockCode:
 
     method = cv.ADAPTIVE_THRESH_GAUSSIAN_C if typ == 0 else cv.ADAPTIVE_THRESH_MEAN_C
     thresh = cv.adaptiveThreshold(img, 256, method, cv.THRESH_BINARY, kernel, weight)
-    #element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_element, kernel_element))
-    #thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, element)
-    return cv.cvtColor(thresh, cv.COLOR_GRAY2BGR)
+    element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_element, kernel_element))
+    thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, element)
+    thresh = cv.morphologyEx(thresh, cv.MORPH_ERODE, element)
 
+    thresh2 = cv.morphologyEx(thresh, cv.MORPH_ERODE, element, iterations=20)
+
+    thresh2 = cv.bitwise_not(thresh2)
+    _, contours, _ = cv.findContours(thresh2, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
+    sizes = [(cv.contourArea(contour), contour) for contour in contours]
+
+    thresh_color = cv.cvtColor(thresh, cv.COLOR_GRAY2BGR)
+    if len(sizes) == 0: 
+      return thresh_color
+    high = max(sizes, key=lambda c: c[0])[1]
+
+    rect = cv.minAreaRect(high)
+    box = cv.boxPoints(rect)
+    box = np.int0(box)
+    thresh2 = cv.cvtColor(thresh2, cv.COLOR_GRAY2BGR)
+    thresh2 = thresh_color.copy()
+    cv.drawContours(thresh2, [box], 0, (0,255,0), 2)
+
+    corped, rot = self.corp_image(thresh, rect)
+
+    cv.imshow('Thresh2', thresh2)
+    if np.sum(corped.shape) == 0:
+      return thresh_color
+
+    corped = cv.cvtColor(corped, cv.COLOR_GRAY2BGR)
+    cv.imshow('Corped', corped)
+    return corped
 
 def arg_parse():
   parser = argparse.ArgumentParser('')
@@ -84,9 +169,17 @@ def setup_dnn_pipeline(pipeline):
   pipeline.add_detection(pix2pix)
 
   merger = PipeLine.result_processing.ProcessorMerger()
+  component = PipeLine.result_processing.ProcessorComponents()
+  selector = PipeLine.result_processing.ProcessorSelector()
+  vLists = PipeLine.result_processing.ProcessorLists('v')
+  hLists = PipeLine.result_processing.ProcessorLists('h')
   pipeline.add_processing(merger)
+  pipeline.add_processing(component)
+  pipeline.add_processing(selector)
+  pipeline.add_processing(vLists)
+  pipeline.add_processing(hLists)
 
-  html_generator = PipeLine.code_generation.GeneratorHTML()
+  html_generator = PipeLine.code_generation.GeneratorHTMLGRIDV2()
   pipeline.add_generator(html_generator)
 
 def setup_cnn_pipeline(pipeline, classes_path):
@@ -94,9 +187,17 @@ def setup_cnn_pipeline(pipeline, classes_path):
   pipeline.add_detection(cnn)
 
   merger = PipeLine.result_processing.ProcessorMerger()
+  component = PipeLine.result_processing.ProcessorComponents()
+  selector = PipeLine.result_processing.ProcessorSelector()
+  vLists = PipeLine.result_processing.ProcessorLists('v')
+  hLists = PipeLine.result_processing.ProcessorLists('h')
   pipeline.add_processing(merger)
-
-  html_generator = PipeLine.code_generation.GeneratorHTML()
+  pipeline.add_processing(component)
+  pipeline.add_processing(selector)
+  pipeline.add_processing(vLists)
+  pipeline.add_processing(hLists)  
+  
+  html_generator = PipeLine.code_generation.GeneratorHTMLGRIDV2()
   pipeline.add_generator(html_generator)
 
 def setup_debug_gen_pipeline(pipeline, annotation_file):
@@ -107,13 +208,26 @@ def setup_debug_gen_pipeline(pipeline, annotation_file):
   pipeline.ommit_process('detection', objs)
 
   merger = PipeLine.result_processing.ProcessorMerger()
+  component = PipeLine.result_processing.ProcessorComponents()
+  selector = PipeLine.result_processing.ProcessorSelector()
+  vLists = PipeLine.result_processing.ProcessorLists('v')
+  hLists = PipeLine.result_processing.ProcessorLists('h')
   pipeline.add_processing(merger)
+  pipeline.add_processing(component)
+  pipeline.add_processing(selector)
+  pipeline.add_processing(vLists)
+  pipeline.add_processing(hLists)
 
-  html_generator = PipeLine.code_generation.GeneratorHTML()
+  html_generator = PipeLine.code_generation.GeneratorHTMLLists()
+  html_generator = PipeLine.code_generation.GeneratorHTMLGRIDV2()
   pipeline.add_generator(html_generator)
 
 def main():
   args = arg_parse()
+
+  print(BANNER, flush=True)
+  print(SETUP, flush=True)
+
   pipeline = PipeLine.Pipeline(args.out)
 
   if not args.debuggen is None:
@@ -129,8 +243,9 @@ def main():
     mockcode.video()
   else:
     mockcode.image(args.image)
-  
-  cv.waitKey(0)
+
+  #cv.waitKey(0)
+  input('...')  
 
 if __name__ == "__main__":
   main()
