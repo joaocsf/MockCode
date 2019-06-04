0754f9acff93e68b307d4580b425bb5b818dd920
João Ferreira <noobcsf@hotmail.com>
2019-06-04 17:02:32 +0100

    Added Test Generator

--- pipeline/code_generation/__init__.py
+++ pipeline/code_generation/__init__.py
@@ -3,3 +3,4 @@ from .GeneratorHTML import GeneratorHTML
 from .GeneratorHTMLLists import GeneratorHTMLLists
 from .GeneratorHTMLGRID import GeneratorHTMLGRID
 from .GeneratorHTMLGRIDV2 import GeneratorHTMLGRIDV2
+from .GeneratorTest import GeneratorTest
