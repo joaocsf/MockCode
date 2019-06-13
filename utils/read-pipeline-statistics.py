03ddfd4c2beb937f8a434e36d61621f1c70b8fbe
João Ferreira <noobcsf@hotmail.com>
2019-06-13 22:46:30 +0100

    Added Option to print the total

--- utils/read-pipeline-statistics.py
+++ utils/read-pipeline-statistics.py
@@ -4,14 +4,24 @@ import matplotlib.pyplot as plt
 import os
 out_path = './results'
 import json
+import numpy as np
 
 def read_file(file_path):
   objs = load_history(file_path)  
   os.makedirs(out_path, exist_ok=True)
 
-
+  total = []
   for obj in objs:
-    makeChart(obj)
+    tmp = makeChart(obj)
+    print(len(tmp), flush=True)
+    if len(total) == 0:
+      total = tmp
+    else:
+      for i, t in enumerate(tmp):
+        total[i] += t
+  
+  plot_history_charts('Time', [['',total, np.average(total)]], 'Total')
+
 
 def makeChart(obj):
   chart_title = '{0} (Average: {1:.3f}s)'.format(obj['name'], obj['average'])
@@ -33,11 +43,14 @@ def makeChart(obj):
   if len(obj['childs']) > 1:
     obj = ['Total', commulative, obj['average']]
     data = [obj] + data
+  
+  res = data[0][1]
   #data.append(['Total', commulative, obj['average']])
 
   plot_history_charts('Time', data, chart_title)
 
   store_string(chart_title, data)
+  return res
 
 def store_string(title, data):
   title = title.replace('(','').replace(')','').replace('_','').replace(':','').replace('.','-')
@@ -56,6 +69,7 @@ def plot_history_charts(yLabel, data, title):
   plt.ylabel(yLabel)
   plt.legend(handles=handles)
   plt.title(title)
+  #plt.xscale('log',basex=2)
 
   title = title.replace('(','').replace(')','').replace('_','').replace(':','').replace('.','-')
   path = os.path.join(out_path, '{0}.pdf'.format(title))
