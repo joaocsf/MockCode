b19a9dfc154fbb65c56648029cd0c34f15910ae9
João Ferreira <noobcsf@hotmail.com>
2019-06-13 22:40:33 +0100

    Added Hierarchy Calculation

--- pipeline/result_processing/__init__.py
+++ pipeline/result_processing/__init__.py
@@ -2,4 +2,5 @@ from .ProcessorMerger import ProcessorMerger
 from .ProcessorSelector import ProcessorSelector
 from .ProcessorLists import ProcessorLists
 from .ProcessorComponents import ProcessorComponents
+from .ProcessorHierarchy import ProcessorHierarchy
 from .Processor import Processor
